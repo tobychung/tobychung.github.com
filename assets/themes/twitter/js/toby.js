8438f34bb818095b1094be1e41d2d4852f16e110
tobychung <toby4120@gmail.com>
2014-12-23 13:32:49 +0800

    responsive for mobile done

---
+++ assets/themes/twitter/js/toby.js
@@ -0,0 +1,19 @@
+$(function(){
+
+	var $window = $(window);
+
+	$window.resize(function(){	
+		
+		var contact = $(".contact");
+		var contactX = contact.offset().left;
+		var plus = $('.plus_all');
+		var plusX = contactX + 230 + 'px';
+			
+		plus.css({left:plusX});
+
+	});
+	
+
+
+
+});
