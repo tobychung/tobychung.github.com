e98b4d4b3a300d86a1b6f99cc1563d070bdf57ea
tobychung <toby4120@gmail.com>
2014-12-23 14:51:02 +0800

    index init done

--- assets/themes/twitter/js/toby.js
+++ assets/themes/twitter/js/toby.js
@@ -1,7 +1,14 @@
 $(function(){
 
 	var $window = $(window);
+	var contact = $(".contact");
+	var contactX = contact.offset().left;
+	var plus = $('.plus_all');
+	var plusX = contactX + 230 + 'px';
+		
+	plus.css({left:plusX});
 
+	
 	$window.resize(function(){	
 		
 		var contact = $(".contact");
