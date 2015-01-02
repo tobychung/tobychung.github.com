3bd7d775b3008843613aedb0d019c5503202e653
tobychung <toby4120@gmail.com>
2015-01-02 15:47:27 +0800

    background modified

--- assets/themes/twitter/js/toby.js
+++ assets/themes/twitter/js/toby.js
@@ -1,24 +1,24 @@
 $(function(){
 
-	var $window = $(window);
-	var contact = $(".contact");
-	var contactX = contact.offset().left;
-	var plus = $('.plus_all');
-	var plusX = contactX + 230 + 'px';
+	// var $window = $(window);
+	// var contact = $(".contact");
+	// var contactX = contact.offset().left;
+	// var plus = $('.plus_all');
+	// var plusX = contactX + 230 + 'px';
 		
-	plus.css({left:plusX});
+	// plus.css({left:plusX});
 
 	
-	$window.resize(function(){	
+	// $window.resize(function(){	
 		
-		var contact = $(".contact");
-		var contactX = contact.offset().left;
-		var plus = $('.plus_all');
-		var plusX = contactX + 230 + 'px';
+	// 	var contact = $(".contact");
+	// 	var contactX = contact.offset().left;
+	// 	var plus = $('.plus_all');
+	// 	var plusX = contactX + 230 + 'px';
 			
-		plus.css({left:plusX});
+	// 	plus.css({left:plusX});
 
-	});
+	// });
 	
 
 
--- index.md
+++ index.md
@@ -5,14 +5,12 @@ tagline:
 ---
 {% include JB/setup %}
 
-哈囉～我是Toby
+哈囉～
 
-以前綽號叫居居，養的狗叫歪歪
+我是Toby
 
-喜歡
-[Iggy](https://www.youtube.com/watch?v=u3u22OYqFGo#t=1m41s)
+養的狗叫歪歪
 
-Thats why！！
 
 <div class="plus_all"></div>
 
@@ -22,9 +20,13 @@ Thats why！！
 
 也在持續燃燒<mark>我的小宇宙</mark>當中
 
+<br/>
+
 這裡是我紀錄學習筆記及分享生活的小小天地
 
 希望可以幫助到<mark>碰到一樣問題的朋友</mark>
+
+<br/>
 
 有問題歡迎<mark>聯絡我</mark>一起討論來腦力激盪一下xD
 
