ca57e7bc77f98551abb251d5e7b51799d4959d05
tobychung <toby4120@gmail.com>
2015-01-03 16:39:49 +0800

    huge changed

--- assets/themes/twitter/js/toby.js
+++ assets/themes/twitter/js/toby.js
@@ -1,10 +1,37 @@
 $(function(){
 
 
-		$('.header_title').addClass('fadeInDown animated');
-	
+	var $window = $(window);
 
-//.find('.header_text').andSelf()
+
+//------ 偵測scroll ------//
+	$window.on('scroll',function(e){
+		
+		
+		var pos = $window.scrollTop();
+
+		if(pos>=400){
+
+			$('.header-wrap').css({display:'none'});
+		}
+
+		else{
+
+			$('.header-wrap').css({display:''});
+		}
+
+
+
+		console.log('現在滾了',pos,'px');
+
+	});
+
+
+//------ 歪歪前端進場 ------//
+
+	$('.header_title').addClass('fadeInDown animated');
+
+//------ code類型 ------//
 
 
 	var highlight_width = $('.highlight').width();
